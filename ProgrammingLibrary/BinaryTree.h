506713e8161f1845aba6ada983e3f3e4c8333bc4
BenceDeveloper <bencebankiit@gmail.com>
2024-08-27 16:23:05 +0200

    BinaryTree

---
+++ ProgrammingLibrary/BinaryTree.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Node
+{
+	public:
+		int data;
+		Node *Right;
+		Node *Left;
+
+	Node(int value)
+	{
+		data = value;
+		Left = Right = nullptr;
+	}
+
+};
+
+class BinaryTree
+{
+	public:
+		Node* Root;
+
+		BinaryTree()
+		{
+			Root = nullptr;
+		}
+		
+		Node* Insert(Node* node, int value)
+		{
+			if(node == nullptr)
+			{
+				return new Node(value);
+			}
+			if(value < node -> data)
+			{
+				node->Left = Insert(node->Left, value);
+			}
+			else
+			{
+				node->Right = Insert(node->Right, value);
+			}
+			return node;
+		}
+		void Inorder(Node* node) 
+		{
+			if (node != nullptr) 
+			{
+				Inorder(node->Left);
+				cout << node->data << " ";
+				Inorder(node->Right);
+			}
+		}
+};
